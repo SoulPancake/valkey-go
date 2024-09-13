redis/rueidis

feat: add TxPipeline to rueidiscompat (#605)
* feat: add TxPipeline to rueidiscompat

Signed-off-by: Rueian <rueiancsie@gmail.com>

* feat: alias rueidiscompat.Nil to rueidis.Nil

* feat: add todos to the rueidiscompat cmdable interface

Signed-off-by: Rueian <rueiancsie@gmail.com>

---------

Signed-off-by: Rueian <rueiancsie@gmail.com>

rueidiscompat/tx.go
@@ -1,0 +1,145 @@
+package rueidiscompat
+
+import (
+	"context"
+	"errors"
+	"time"
+	"unsafe"
+
+	"github.com/redis/rueidis"
+)
+
+var TxFailedErr = errors.New("redis: transaction failed")
+
+var _ Pipeliner = (*TxPipeline)(nil)
+
+type rePipeline = Pipeline
+
+func newTxPipeline(real rueidis.Client) *TxPipeline {
+	return &TxPipeline{rePipeline: newPipeline(real)}
+}
+
+type TxPipeline struct {
+	*rePipeline
+}
+
+func (c *TxPipeline) Exec(ctx context.Context) ([]Cmder, error) {
+	p := c.comp.client.(*proxy)
+	if len(p.cmds) == 0 {
+		return nil, nil
+	}
+
+	rets := c.rets
+	cmds := p.cmds
+	c.rets = nil
+	p.cmds = nil
+
+	cmds = append(cmds, c.comp.client.B().Multi().Build(), c.comp.client.B().Exec().Build())
+	for i := len(cmds) - 2; i >= 1; i-- {
+		j := i - 1
+		cmds[j], cmds[i] = cmds[i], cmds[j]
+	}
+
+	resp := p.DoMulti(ctx, cmds...)
+	results, err := resp[len(resp)-1].ToArray()
+	if rueidis.IsRedisNil(err) {
+		err = TxFailedErr
+	}
+	for i, r := range results {
+		rets[i].from(*(*rueidis.RedisResult)(unsafe.Pointer(&proxyresult{
+			err: resp[i+1].NonRedisError(),
+			val: r,
+		})))
+	}
+	return rets, err
+}
+
+func (c *TxPipeline) Pipelined(ctx context.Context, fn func(Pipeliner) error) ([]Cmder, error) {
+	if err := fn(c); err != nil {
+		return nil, err
+	}
+	return c.Exec(ctx)
+}
+
+func (c *TxPipeline) Pipeline() Pipeliner {
+	return c
+}
+
+func (c *TxPipeline) TxPipelined(ctx context.Context, fn func(Pipeliner) error) ([]Cmder, error) {
+	return c.Pipelined(ctx, fn)
+}
+
+func (c *TxPipeline) TxPipeline() Pipeliner {
+	return c
+}
+
+var _ rueidis.Client = (*txproxy)(nil)
+
+type txproxy struct {
+	rueidis.CoreClient
+}
+
+func (p *txproxy) DoCache(_ context.Context, _ rueidis.Cacheable, _ time.Duration) (resp rueidis.RedisResult) {
+	panic("not implemented")
+}
+
+func (p *txproxy) DoMultiCache(_ context.Context, _ ...rueidis.CacheableTTL) (resp []rueidis.RedisResult) {
+	panic("not implemented")
+}
+
+func (p *txproxy) DoStream(_ context.Context, _ rueidis.Completed) rueidis.RedisResultStream {
+	panic("not implemented")
+}
+
+func (p *txproxy) DoMultiStream(_ context.Context, _ ...rueidis.Completed) rueidis.MultiRedisResultStream {
+	panic("not implemented")
+}
+
+func (p *txproxy) Dedicated(_ func(rueidis.DedicatedClient) error) (err error) {
+	panic("not implemented")
+}
+
+func (p *txproxy) Dedicate() (client rueidis.DedicatedClient, cancel func()) {
+	panic("not implemented")
+}
+
+func (p *txproxy) Nodes() map[string]rueidis.Client {
+	panic("not implemented")
+}
+
+type Tx interface {
+	CoreCmdable
+	Watch(ctx context.Context, keys ...string) *StatusCmd
+	Unwatch(ctx context.Context, keys ...string) *StatusCmd
+	Close(ctx context.Context) error
+}
+
+func newTx(client rueidis.DedicatedClient, cancel func()) *tx {
+	return &tx{CoreCmdable: NewAdapter(&txproxy{CoreClient: client}), cancel: cancel}
+}
+
+type tx struct {
+	CoreCmdable
+	cancel func()
+}
+
+func (t *tx) Watch(ctx context.Context, keys ...string) *StatusCmd {
+	ret := &StatusCmd{}
+	if len(keys) != 0 {
+		client := t.CoreCmdable.(*Compat).client
+		ret.from(client.Do(ctx, client.B().Watch().Key(keys...).Build()))
+	}
+	return ret
+}
+
+func (t *tx) Unwatch(ctx context.Context, _ ...string) *StatusCmd {
+	ret := &StatusCmd{}
+	client := t.CoreCmdable.(*Compat).client
+	ret.from(client.Do(ctx, client.B().Unwatch().Build()))
+	return ret
+}
+
+func (t *tx) Close(_ context.Context) error {
+	t.cancel()
+	return nil
+}

rueidiscompat/tx_test.go
@@ -1,0 +1,168 @@
+// Copyright (c) 2013 The github.com/go-redis/redis Authors.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// * Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+// * Redistributions in binary form must reproduce the above
+// copyright notice, this list of conditions and the following disclaimer
+// in the documentation and/or other materials provided with the
+// distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+package rueidiscompat
+
+import (
+	"errors"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/redis/rueidis"
+)
+
+var _ = Describe("RESP3 TxPipeline Commands", func() {
+	testAdapterTxPipeline(true)
+})
+
+var _ = Describe("RESP2 TxPipeline Commands", func() {
+	testAdapterTxPipeline(false)
+})
+
+func testAdapterTxPipeline(resp3 bool) {
+	var adapter Cmdable
+
+	BeforeEach(func() {
+		if resp3 {
+			adapter = adapterresp3
+		} else {
+			adapter = adapterresp2
+		}
+		Expect(adapter.FlushDB(ctx).Err()).NotTo(HaveOccurred())
+		Expect(adapter.FlushAll(ctx).Err()).NotTo(HaveOccurred())
+	})
+
+	It("should TxPipelined", func() {
+		var echo, ping *StringCmd
+		rets, err := adapter.TxPipelined(ctx, func(pipe Pipeliner) error {
+			echo = pipe.Echo(ctx, "hello")
+			ping = pipe.Ping(ctx)
+			Expect(echo.Err()).To(MatchError(placeholder.err))
+			Expect(ping.Err()).To(MatchError(placeholder.err))
+			return nil
+		})
+		Expect(err).NotTo(HaveOccurred())
+		Expect(rets).To(HaveLen(2))
+		Expect(rets[0]).To(Equal(echo))
+		Expect(rets[1]).To(Equal(ping))
+		Expect(echo.Err()).NotTo(HaveOccurred())
+		Expect(echo.Val()).To(Equal("hello"))
+		Expect(ping.Err()).NotTo(HaveOccurred())
+		Expect(ping.Val()).To(Equal("PONG"))
+	})
+
+	It("should TxPipeline", func() {
+		pipe := adapter.TxPipeline()
+		echo := pipe.Echo(ctx, "hello")
+		ping := pipe.Ping(ctx)
+		Expect(echo.Err()).To(MatchError(placeholder.err))
+		Expect(ping.Err()).To(MatchError(placeholder.err))
+		Expect(pipe.Len()).To(Equal(2))
+
+		rets, err := pipe.Exec(ctx)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(pipe.Len()).To(Equal(0))
+		Expect(rets).To(HaveLen(2))
+		Expect(rets[0]).To(Equal(echo))
+		Expect(rets[1]).To(Equal(ping))
+		Expect(echo.Err()).NotTo(HaveOccurred())
+		Expect(echo.Val()).To(Equal("hello"))
+		Expect(ping.Err()).NotTo(HaveOccurred())
+		Expect(ping.Val()).To(Equal("PONG"))
+	})
+
+	It("should Discard", func() {
+		pipe := adapter.TxPipeline()
+		echo := pipe.Echo(ctx, "hello")
+		ping := pipe.Ping(ctx)
+
+		pipe.Discard()
+		Expect(pipe.Len()).To(Equal(0))
+
+		rets, err := pipe.Exec(ctx)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(rets).To(HaveLen(0))
+
+		Expect(echo.Err()).To(MatchError(placeholder.err))
+		Expect(ping.Err()).To(MatchError(placeholder.err))
+	})
+
+	It("should Watch", func() {
+		k1 := "_k1_"
+		k2 := "_k2_"
+		err := adapter.Watch(ctx, func(t Tx) error {
+			if t.Get(ctx, k1).Err() != Nil {
+				return errors.New("unclean")
+			}
+			if t.Get(ctx, k2).Err() != Nil {
+				return errors.New("unclean")
+			}
+			_, err := t.TxPipelined(ctx, func(pipe Pipeliner) error {
+				pipe.Set(ctx, k1, k1, 0)
+				pipe.Set(ctx, k2, k2, 0)
+				return nil
+			})
+			return err
+		}, k1, k2)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(adapter.Get(ctx, k1).Val()).To(Equal(k1))
+		Expect(adapter.Get(ctx, k2).Val()).To(Equal(k2))
+	})
+
+	It("should Watch Abort", func() {
+		k1 := "_k1_"
+		ch := make(chan error)
+		go func() {
+			ch <- adapter.Watch(ctx, func(t Tx) error {
+				ch <- nil
+				<-ch
+				_, err := t.TxPipelined(ctx, func(pipe Pipeliner) error {
+					pipe.Del(ctx, k1)
+					return nil
+				})
+				return err
+			}, k1)
+		}()
+		<-ch
+		Expect(adapter.Set(ctx, k1, k1, 0).Err()).NotTo(HaveOccurred())
+		ch <- nil
+		Expect(<-ch).To(MatchError(TxFailedErr))
+	})
+
+	It("should Unwatch and Close", func() {
+		k1 := "_k1_"
+		err := adapter.Watch(ctx, func(t Tx) error {
+			Expect(t.Unwatch(ctx).Err()).NotTo(HaveOccurred())
+			Expect(t.Close(ctx)).NotTo(HaveOccurred())
+			_, err := t.TxPipelined(ctx, func(pipe Pipeliner) error {
+				pipe.Del(ctx, k1)
+				return nil
+			})
+			return err
+		}, k1)
+		Expect(err).To(MatchError(rueidis.ErrDedicatedClientRecycled))
+	})
+}

SoulPancake/valkey-go

valkeycompat/tx.go
@@ -1,0 +1,145 @@
+package valkeycompat
+
+import (
+	"context"
+	"errors"
+	"time"
+	"unsafe"
+
+	"github.com/valkey-io/valkey-go"
+)
+
+var TxFailedErr = errors.New("valkey: transaction failed")
+
+var _ Pipeliner = (*TxPipeline)(nil)
+
+type rePipeline = Pipeline
+
+func newTxPipeline(real valkey.Client) *TxPipeline {
+	return &TxPipeline{rePipeline: newPipeline(real)}
+}
+
+type TxPipeline struct {
+	*rePipeline
+}
+
+func (c *TxPipeline) Exec(ctx context.Context) ([]Cmder, error) {
+	p := c.comp.client.(*proxy)
+	if len(p.cmds) == 0 {
+		return nil, nil
+	}
+
+	rets := c.rets
+	cmds := p.cmds
+	c.rets = nil
+	p.cmds = nil
+
+	cmds = append(cmds, c.comp.client.B().Multi().Build(), c.comp.client.B().Exec().Build())
+	for i := len(cmds) - 2; i >= 1; i-- {
+		j := i - 1
+		cmds[j], cmds[i] = cmds[i], cmds[j]
+	}
+
+	resp := p.DoMulti(ctx, cmds...)
+	results, err := resp[len(resp)-1].ToArray()
+	if valkey.IsValkeyNil(err) {
+		err = TxFailedErr
+	}
+	for i, r := range results {
+		rets[i].from(*(*valkey.ValkeyResult)(unsafe.Pointer(&proxyresult{
+			err: resp[i+1].NonValkeyError(),
+			val: r,
+		})))
+	}
+	return rets, err
+}
+
+func (c *TxPipeline) Pipelined(ctx context.Context, fn func(Pipeliner) error) ([]Cmder, error) {
+	if err := fn(c); err != nil {
+		return nil, err
+	}
+	return c.Exec(ctx)
+}
+
+func (c *TxPipeline) Pipeline() Pipeliner {
+	return c
+}
+
+func (c *TxPipeline) TxPipelined(ctx context.Context, fn func(Pipeliner) error) ([]Cmder, error) {
+	return c.Pipelined(ctx, fn)
+}
+
+func (c *TxPipeline) TxPipeline() Pipeliner {
+	return c
+}
+
+var _ valkey.Client = (*txproxy)(nil)
+
+type txproxy struct {
+	valkey.CoreClient
+}
+
+func (p *txproxy) DoCache(_ context.Context, _ valkey.Cacheable, _ time.Duration) (resp valkey.ValkeyResult) {
+	panic("not implemented")
+}
+
+func (p *txproxy) DoMultiCache(_ context.Context, _ ...valkey.CacheableTTL) (resp []valkey.ValkeyResult) {
+	panic("not implemented")
+}
+
+func (p *txproxy) DoStream(_ context.Context, _ valkey.Completed) valkey.ValkeyResultStream {
+	panic("not implemented")
+}
+
+func (p *txproxy) DoMultiStream(_ context.Context, _ ...valkey.Completed) valkey.MultiValkeyResultStream {
+	panic("not implemented")
+}
+
+func (p *txproxy) Dedicated(_ func(valkey.DedicatedClient) error) (err error) {
+	panic("not implemented")
+}
+
+func (p *txproxy) Dedicate() (client valkey.DedicatedClient, cancel func()) {
+	panic("not implemented")
+}
+
+func (p *txproxy) Nodes() map[string]valkey.Client {
+	panic("not implemented")
+}
+
+type Tx interface {
+	CoreCmdable
+	Watch(ctx context.Context, keys ...string) *StatusCmd
+	Unwatch(ctx context.Context, keys ...string) *StatusCmd
+	Close(ctx context.Context) error
+}
+
+func newTx(client valkey.DedicatedClient, cancel func()) *tx {
+	return &tx{CoreCmdable: NewAdapter(&txproxy{CoreClient: client}), cancel: cancel}
+}
+
+type tx struct {
+	CoreCmdable
+	cancel func()
+}
+
+func (t *tx) Watch(ctx context.Context, keys ...string) *StatusCmd {
+	ret := &StatusCmd{}
+	if len(keys) != 0 {
+		client := t.CoreCmdable.(*Compat).client
+		ret.from(client.Do(ctx, client.B().Watch().Key(keys...).Build()))
+	}
+	return ret
+}
+
+func (t *tx) Unwatch(ctx context.Context, _ ...string) *StatusCmd {
+	ret := &StatusCmd{}
+	client := t.CoreCmdable.(*Compat).client
+	ret.from(client.Do(ctx, client.B().Unwatch().Build()))
+	return ret
+}
+
+func (t *tx) Close(_ context.Context) error {
+	t.cancel()
+	return nil
+}

valkeycompat/tx_test.go
@@ -1,0 +1,168 @@
+// Copyright (c) 2013 The github.com/go-redis/redis Authors.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// * Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+// * Redistributions in binary form must reproduce the above
+// copyright notice, this list of conditions and the following disclaimer
+// in the documentation and/or other materials provided with the
+// distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+package valkeycompat
+
+import (
+	"errors"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/valkey-io/valkey-go"
+)
+
+var _ = Describe("RESP3 TxPipeline Commands", func() {
+	testAdapterTxPipeline(true)
+})
+
+var _ = Describe("RESP2 TxPipeline Commands", func() {
+	testAdapterTxPipeline(false)
+})
+
+func testAdapterTxPipeline(resp3 bool) {
+	var adapter Cmdable
+
+	BeforeEach(func() {
+		if resp3 {
+			adapter = adapterresp3
+		} else {
+			adapter = adapterresp2
+		}
+		Expect(adapter.FlushDB(ctx).Err()).NotTo(HaveOccurred())
+		Expect(adapter.FlushAll(ctx).Err()).NotTo(HaveOccurred())
+	})
+
+	It("should TxPipelined", func() {
+		var echo, ping *StringCmd
+		rets, err := adapter.TxPipelined(ctx, func(pipe Pipeliner) error {
+			echo = pipe.Echo(ctx, "hello")
+			ping = pipe.Ping(ctx)
+			Expect(echo.Err()).To(MatchError(placeholder.err))
+			Expect(ping.Err()).To(MatchError(placeholder.err))
+			return nil
+		})
+		Expect(err).NotTo(HaveOccurred())
+		Expect(rets).To(HaveLen(2))
+		Expect(rets[0]).To(Equal(echo))
+		Expect(rets[1]).To(Equal(ping))
+		Expect(echo.Err()).NotTo(HaveOccurred())
+		Expect(echo.Val()).To(Equal("hello"))
+		Expect(ping.Err()).NotTo(HaveOccurred())
+		Expect(ping.Val()).To(Equal("PONG"))
+	})
+
+	It("should TxPipeline", func() {
+		pipe := adapter.TxPipeline()
+		echo := pipe.Echo(ctx, "hello")
+		ping := pipe.Ping(ctx)
+		Expect(echo.Err()).To(MatchError(placeholder.err))
+		Expect(ping.Err()).To(MatchError(placeholder.err))
+		Expect(pipe.Len()).To(Equal(2))
+
+		rets, err := pipe.Exec(ctx)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(pipe.Len()).To(Equal(0))
+		Expect(rets).To(HaveLen(2))
+		Expect(rets[0]).To(Equal(echo))
+		Expect(rets[1]).To(Equal(ping))
+		Expect(echo.Err()).NotTo(HaveOccurred())
+		Expect(echo.Val()).To(Equal("hello"))
+		Expect(ping.Err()).NotTo(HaveOccurred())
+		Expect(ping.Val()).To(Equal("PONG"))
+	})
+
+	It("should Discard", func() {
+		pipe := adapter.TxPipeline()
+		echo := pipe.Echo(ctx, "hello")
+		ping := pipe.Ping(ctx)
+
+		pipe.Discard()
+		Expect(pipe.Len()).To(Equal(0))
+
+		rets, err := pipe.Exec(ctx)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(rets).To(HaveLen(0))
+
+		Expect(echo.Err()).To(MatchError(placeholder.err))
+		Expect(ping.Err()).To(MatchError(placeholder.err))
+	})
+
+	It("should Watch", func() {
+		k1 := "_k1_"
+		k2 := "_k2_"
+		err := adapter.Watch(ctx, func(t Tx) error {
+			if t.Get(ctx, k1).Err() != Nil {
+				return errors.New("unclean")
+			}
+			if t.Get(ctx, k2).Err() != Nil {
+				return errors.New("unclean")
+			}
+			_, err := t.TxPipelined(ctx, func(pipe Pipeliner) error {
+				pipe.Set(ctx, k1, k1, 0)
+				pipe.Set(ctx, k2, k2, 0)
+				return nil
+			})
+			return err
+		}, k1, k2)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(adapter.Get(ctx, k1).Val()).To(Equal(k1))
+		Expect(adapter.Get(ctx, k2).Val()).To(Equal(k2))
+	})
+
+	It("should Watch Abort", func() {
+		k1 := "_k1_"
+		ch := make(chan error)
+		go func() {
+			ch <- adapter.Watch(ctx, func(t Tx) error {
+				ch <- nil
+				<-ch
+				_, err := t.TxPipelined(ctx, func(pipe Pipeliner) error {
+					pipe.Del(ctx, k1)
+					return nil
+				})
+				return err
+			}, k1)
+		}()
+		<-ch
+		Expect(adapter.Set(ctx, k1, k1, 0).Err()).NotTo(HaveOccurred())
+		ch <- nil
+		Expect(<-ch).To(MatchError(TxFailedErr))
+	})
+
+	It("should Unwatch and Close", func() {
+		k1 := "_k1_"
+		err := adapter.Watch(ctx, func(t Tx) error {
+			Expect(t.Unwatch(ctx).Err()).NotTo(HaveOccurred())
+			Expect(t.Close(ctx)).NotTo(HaveOccurred())
+			_, err := t.TxPipelined(ctx, func(pipe Pipeliner) error {
+				pipe.Del(ctx, k1)
+				return nil
+			})
+			return err
+		}, k1)
+		Expect(err).To(MatchError(valkey.ErrDedicatedClientRecycled))
+	})
+}